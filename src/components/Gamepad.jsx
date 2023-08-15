2bd0494cac066f7bc81036cef15d45d1c7c8ed34
umair <muhammadumair2511@gmail.com>
2023-08-15 16:01:09 +0500

    changed gamepad layout

--- src/components/Gamepad.jsx
+++ src/components/Gamepad.jsx
@@ -2,30 +2,30 @@ import Arrow from "../assets/arrow.svg";
 
 const Gamepad = ({ updateLastMoveRef }) => {
   return (
-    <div className="flex w-11/12 md:w-[35rem] m-auto">
+    <div className="grid grid-cols-3 grid-rows-3 w-1/2 md:w-[35rem] m-auto border-clr-900 border-y-4">
       <button
         onClick={() => updateLastMoveRef("a")}
-        className="bg-clr-950 p-2 flex-1 active:bg-clr-900"
+        className="bg-clr-950 p-2 flex-1 active:bg-clr-900 col-start-1 row-start-2"
       >
-        <img src={Arrow} alt="Left" className=" -rotate-90 w-8 m-auto " />
+        <img src={Arrow} alt="Left" className=" -rotate-90 w-5 m-auto " />
       </button>
       <button
         onClick={() => updateLastMoveRef("d")}
-        className="bg-clr-950 p-2 flex-1 active:bg-clr-900"
+        className="bg-clr-950 p-2 flex-1 active:bg-clr-900 col-start-3 row-start-2"
       >
-        <img src={Arrow} alt="Right" className=" rotate-90 w-8 m-auto" />
+        <img src={Arrow} alt="Right" className=" rotate-90 w-5 m-auto" />
       </button>
       <button
         onClick={() => updateLastMoveRef("w")}
-        className="bg-clr-950 p-2 flex-1 active:bg-clr-900"
+        className="bg-clr-950 p-2 flex-1 active:bg-clr-900 col-start-2 row-start-1"
       >
-        <img src={Arrow} alt="Up" className=" rotate-0 w-8 m-auto" />
+        <img src={Arrow} alt="Up" className=" rotate-0 w-5 m-auto" />
       </button>
       <button
         onClick={() => updateLastMoveRef("s")}
-        className="bg-clr-950 p-2 flex-1 active:bg-clr-900"
+        className="bg-clr-950 p-2 flex-1 active:bg-clr-900 col-start-2 row-start-3"
       >
-        <img src={Arrow} alt="Down" className=" rotate-180 w-8 m-auto" />
+        <img src={Arrow} alt="Down" className=" rotate-180 w-5 m-auto" />
       </button>
     </div>
   );
