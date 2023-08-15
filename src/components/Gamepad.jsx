1b3fea48848cef1da3aa23ace1113e946c1bd2f1
umair <muhammadumair2511@gmail.com>
2023-08-15 15:44:13 +0500

    added styling to gamepad

--- src/components/Gamepad.jsx
+++ src/components/Gamepad.jsx
@@ -5,25 +5,25 @@ const Gamepad = ({ updateLastMoveRef }) => {
     <div className="flex w-11/12 md:w-[35rem] m-auto">
       <button
         onClick={() => updateLastMoveRef("a")}
-        className="bg-clr-950 p-2 flex-1"
+        className="bg-clr-950 p-2 flex-1 active:bg-clr-900"
       >
-        <img src={Arrow} alt="Left" className=" -rotate-90 w-8 m-auto" />
+        <img src={Arrow} alt="Left" className=" -rotate-90 w-8 m-auto " />
       </button>
       <button
         onClick={() => updateLastMoveRef("d")}
-        className="bg-clr-950 p-2 flex-1"
+        className="bg-clr-950 p-2 flex-1 active:bg-clr-900"
       >
         <img src={Arrow} alt="Right" className=" rotate-90 w-8 m-auto" />
       </button>
       <button
         onClick={() => updateLastMoveRef("w")}
-        className="bg-clr-950 p-2 flex-1"
+        className="bg-clr-950 p-2 flex-1 active:bg-clr-900"
       >
         <img src={Arrow} alt="Up" className=" rotate-0 w-8 m-auto" />
       </button>
       <button
         onClick={() => updateLastMoveRef("s")}
-        className="bg-clr-950 p-2 flex-1"
+        className="bg-clr-950 p-2 flex-1 active:bg-clr-900"
       >
         <img src={Arrow} alt="Down" className=" rotate-180 w-8 m-auto" />
       </button>
