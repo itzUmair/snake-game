2a1f095482903d6df240ef6bbbc9b8db9aa4f92b
umair <muhammadumair2511@gmail.com>
2023-08-15 16:03:07 +0500

    fixed button size

--- src/components/Gamepad.jsx
+++ src/components/Gamepad.jsx
@@ -7,25 +7,25 @@ const Gamepad = ({ updateLastMoveRef }) => {
         onClick={() => updateLastMoveRef("a")}
         className="bg-clr-950 p-2 flex-1 active:bg-clr-900 col-start-1 row-start-2"
       >
-        <img src={Arrow} alt="Left" className=" -rotate-90 w-5 m-auto " />
+        <img src={Arrow} alt="Left" className=" -rotate-90 w-10 m-auto " />
       </button>
       <button
         onClick={() => updateLastMoveRef("d")}
         className="bg-clr-950 p-2 flex-1 active:bg-clr-900 col-start-3 row-start-2"
       >
-        <img src={Arrow} alt="Right" className=" rotate-90 w-5 m-auto" />
+        <img src={Arrow} alt="Right" className=" rotate-90 w-10 m-auto" />
       </button>
       <button
         onClick={() => updateLastMoveRef("w")}
         className="bg-clr-950 p-2 flex-1 active:bg-clr-900 col-start-2 row-start-1"
       >
-        <img src={Arrow} alt="Up" className=" rotate-0 w-5 m-auto" />
+        <img src={Arrow} alt="Up" className=" rotate-0 w-10 m-auto" />
       </button>
       <button
         onClick={() => updateLastMoveRef("s")}
         className="bg-clr-950 p-2 flex-1 active:bg-clr-900 col-start-2 row-start-3"
       >
-        <img src={Arrow} alt="Down" className=" rotate-180 w-5 m-auto" />
+        <img src={Arrow} alt="Down" className=" rotate-180 w-10 m-auto" />
       </button>
     </div>
   );
