9145678842aab8b095ab1b0b85e154d2558d0fe1
umair <muhammadumair2511@gmail.com>
2023-08-16 10:16:14 +0500

    added clear score button on home screen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,15 @@ StartSFX.load();
 
 const Home = ({ gameSettings, setGameSettings, setIsPlaying }) => {
   const [gameSpeed, setGameSpeed] = useState(3);
+  const [hiScore, setHiScore] = useState(
+    JSON.parse(localStorage.getItem("hi-score"))
+  );
 
   return (
     <div className="px-4 lg:px-16 flex flex-col items-center justify-center">
       <img src={Logo} alt="Snake Retro" className=" w-96" />
       <div className=" w-[80vw] lg:w-[30rem]">
+        <h2 className="text-center text-clr-100 py-8">Hi-score: {hiScore}</h2>
         <label
           htmlFor="length"
           className=" mt-4 text-xs text-clr-100 flex justify-between items-center pr-4"
@@ -42,6 +46,15 @@ const Home = ({ gameSettings, setGameSettings, setIsPlaying }) => {
         className="bg-clr-600 py-2 px-4 mt-4 text-clr-100 "
       >
         Play
+      </button>
+      <button
+        onClick={() => {
+          setHiScore(0);
+          localStorage.setItem("hi-score", JSON.stringify(0));
+        }}
+        className="bg-clr-600 py-2 px-4 mt-4 text-clr-100 "
+      >
+        Clear Hi-Score
       </button>
     </div>
   );
